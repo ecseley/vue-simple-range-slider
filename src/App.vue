557c7e8d11184127c9e33d4f82b7c7bc16a73638
Endre Cseley <ecseley@jacada.com>
2021-04-21 10:59:09 -0400

    passing more props

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,62 @@
 <template>
-    <div dir="ltr" style="margin-left: 100px;font-size: 20px">
-        {{value1}}
-        <VueSimpleRangeSlider
-                style="width: 1000px"
-                :min="0"
-                :max="1000000"
-                :logarithmic="true"
-                v-model="value1"
-                active-bar-color="red"
-                bar-color="blue"
-        />
-        {{value2}}
-        <VueSimpleRangeSlider
-                style="width: 100px"
-                :min="0"
-                :max="40"
-                v-model="value2"
-        />
+	<div dir="ltr" style="margin-left: 100px; font-size: 20px">
+		{{ value1 }}
+		<VueSimpleRangeSlider
+			style="width: 1000px"
+			:min="0"
+			:max="1000000"
+			:logarithmic="true"
+			v-model="value1"
+			active-bar-color="red"
+			bar-color="blue"
+		/>
+		{{ value2 }}
+		<VueSimpleRangeSlider style="width: 100px" :min="0" :max="40" v-model="value2" />
 
-
-        {{value1}}
-        <VueSimpleRangeSlider
-                style="width: 1000px;direction: rtl"
-                :min="0"
-                :max="1000000"
-                :logarithmic="true"
-                v-model="value1"
-                active-bar-color="red"
-                bar-color="blue"
-        />
-        {{value2}}
-        <VueSimpleRangeSlider
-                style="width: 100px;direction: rtl"
-                :min="0"
-                :max="40"
-                v-model="value2"
-        />
-    </div>
+		{{ value1 }}
+		<VueSimpleRangeSlider
+			style="width: 1000px; direction: rtl"
+			:min="0"
+			:max="1000000"
+			:logarithmic="true"
+			v-model="value1"
+			active-bar-color="red"
+			bar-color="blue"
+		/>
+		{{ value2 }}
+		<VueSimpleRangeSlider style="width: 100px; direction: rtl" :min="0" :max="40" v-model="value2" />
+	</div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-    import VueSimpleRangeSlider from './components/VueSimpleRangeSlider.vue';
+import {Prop, Component, Vue} from 'vue-property-decorator';
+import VueSimpleRangeSlider from './components/VueSimpleRangeSlider.vue';
 
-    @Component({
-        components: {
-            VueSimpleRangeSlider,
-        },
-    })
-    export default class App extends Vue {
-        value1 = [0,10];
-        value2 = 3;
+@Component({
+	components: {
+		VueSimpleRangeSlider,
+	},
+})
+export default class App extends Vue {
+	value1 = [0, 10];
+	value2 = 3;
 
-
-    }
+	@Prop({default: 0}) activeWidth?: number;
+}
 </script>
 
 <style lang="scss">
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
 </style>
 <style>
-    input{
-        font-size: 30px;
-    }
+input {
+	font-size: 30px;
+}
 </style>
